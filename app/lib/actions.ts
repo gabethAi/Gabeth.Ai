c12a1d4a4a36a1599217fbdc14a1f341423346d5
ruckyTheWebDesigner <rukewejoseph77@gmail.com>
2023-11-09 06:19:12 +0100

    initialize prisma client and add util query functions

---
+++ app/lib/actions.ts
@@ -0,0 +1,98 @@
+// Import Prisma Client and instantiate it
+import { PrismaClient } from "@prisma/client";
+
+const globalForPrisma = global;
+
+export const prisma = new PrismaClient();
+
+// Get a single chat by its ID
+export async function getChat(chatId: string) {
+  return await prisma.conversation.findUnique({
+    where: { id: chatId },
+    include: {
+      messages: true,
+      user: true,
+    },
+  });
+}
+
+// Get multiple chats for a user by their userID
+export async function getChats(userId: string) {
+  return await prisma.conversation.findMany({
+    where: { userId: userId },
+    include: {
+      messages: true,
+      user: true,
+    },
+  });
+}
+
+// Get a user by their email
+export async function getUser(email: string) {
+  return await prisma.user.findUnique({
+    where: { email: email },
+  });
+}
+
+// function to register a new user
+export async function registerUser({
+  email,
+  username,
+  password,
+}: {
+  email: string;
+  username: string;
+  password: string;
+}) {
+  return await prisma.user.create({
+    data: {
+      email: email,
+      username: username,
+      hashedPassword: password,
+    },
+  });
+}
+
+// function to authenticate user login
+export async function loginUser({
+  email,
+  password,
+}: {
+  email: string;
+  password: string;
+}) {
+  return await prisma.user.findFirst({
+    where: {
+      email: email,
+      hashedPassword: password,
+    },
+  });
+}
+
+// Clears all chats for a user
+export async function clearChats(userId: string) {
+  return await prisma.conversation.deleteMany({
+    where: { userId: userId },
+  });
+}
+
+// Removes a specific chat by its ID
+export async function removeChat(chatId: string) {
+  return await prisma.conversation.delete({
+    where: { id: chatId },
+  });
+}
+
+// async function main() {
+//   console.log("starting");
+// }
+
+// main()
+//   .then(async () => {
+//     await prisma.$disconnect();
+//   })
+//   .catch(async (e) => {
+//     console.error(e);
+//     await prisma.$disconnect();
+//     process.exit(1);
+//   });
--- app/lib/next-auth.ts
+++ app/lib/next-auth.ts
@@ -2,6 +2,9 @@ import NextAuth, { type DefaultSession } from "next-auth";
 import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
 import CredentialsProvider from "next-auth/providers/credentials";
+import Credentials from "next-auth/providers/credentials";
+import { getUser, prisma } from "./actions";
+import { Prisma } from "@prisma/client";
 
 declare module "next-auth" {
   interface Session {
@@ -17,6 +20,17 @@ const handler = NextAuth({
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID || "",
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {},
+      async authorize(credentials, req) {
+        const user = await getUser("rcl@gmail.com");
+
+        console.log(user, "user");
+
+        return user;
+      },
     }),
   ],
   pages: {
