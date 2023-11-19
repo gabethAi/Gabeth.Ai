f340649e2a10f7871d4e3dd0ac61073d7894331e
ruckyTheWebDesigner <rukewejoseph77@gmail.com>
2023-11-20 00:04:40 +0100

    Add new tables and columns for user accounts and sessions

--- app/lib/db/schema.ts
+++ app/lib/db/schema.ts
@@ -1,17 +1,23 @@
 import {
   integer,
   pgTable,
+  primaryKey,
   serial,
   text,
   timestamp,
   uniqueIndex,
 } from "drizzle-orm/pg-core";
 
+import type { AdapterAccount } from "@auth/core/adapters";
+
 export const users = pgTable(
   "users",
   {
-    id: serial("id").primaryKey(),
+    id: serial("id").primaryKey().unique(),
+    name: text("name"),
     email: text("email").notNull(),
+    emailVerified: timestamp("emailVerified", { mode: "date" }),
+    image: text("image"),
     createdat: timestamp("createdat").defaultNow().notNull(),
     username: text("username"),
     hashedpassword: text("hashedpassword").notNull(),
@@ -52,3 +58,45 @@ export const reactions = pgTable("reactions", {
   type: text("type").notNull(),
   count: integer("count").default(0).notNull(),
 });
+
+export const accounts = pgTable(
+  "account",
+  {
+    userId: text("userId")
+      .notNull()
+      .references(() => users.id, { onDelete: "cascade" }),
+    type: text("type").$type<AdapterAccount["type"]>().notNull(),
+    provider: text("provider").notNull(),
+    providerAccountId: text("providerAccountId").notNull(),
+    refresh_token: text("refresh_token"),
+    access_token: text("access_token"),
+    expires_at: integer("expires_at"),
+    token_type: text("token_type"),
+    scope: text("scope"),
+    id_token: text("id_token"),
+    session_state: text("session_state"),
+  },
+  (account) => ({
+    compoundKey: primaryKey(account.provider, account.providerAccountId),
+  })
+);
+
+export const sessions = pgTable("session", {
+  sessionToken: text("sessionToken").notNull().primaryKey(),
+  userId: text("userId")
+    .notNull()
+    .references(() => users.id, { onDelete: "cascade" }),
+  expires: timestamp("expires", { mode: "date" }).notNull(),
+});
+
+export const verificationTokens = pgTable(
+  "verificationToken",
+  {
+    identifier: text("identifier").notNull(),
+    token: text("token").notNull(),
+    expires: timestamp("expires", { mode: "date" }).notNull(),
+  },
+  (vt) => ({
+    compoundKey: primaryKey(vt.identifier, vt.token),
+  })
+);
