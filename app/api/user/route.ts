db00d08e9dd568c9018326f38a939cffeb27cfef
ruckyTheWebDesigner <rukewejoseph77@gmail.com>
2023-11-29 02:29:21 +0100

    Add revalidate route and user route

---
+++ app/api/user/route.ts
@@ -0,0 +1,17 @@
+import { getUser } from "@/app/lib/actions";
+import { kv } from "@vercel/kv";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const user = await getUser();
+
+    const data = await kv.get(`user:${user.email}`);
+
+    return NextResponse.json(data);
+  } catch (error: any) {
+    return NextResponse.json({
+      error: "Unauthorized",
+    });
+  }
+}
