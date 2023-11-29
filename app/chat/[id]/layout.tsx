504012cf7da9bf5d1d544c03245bceea86bb67cd
ruckyTheWebDesigner <rukewejoseph77@gmail.com>
2023-11-29 02:28:07 +0100

    Add chat layout components

---
+++ app/chat/[id]/layout.tsx
@@ -0,0 +1,65 @@
+import ChatPanel from "@/app/components/chat/ChatPanel";
+import { getUser } from "@/app/lib/actions";
+import { getChat } from "@/app/utils/actions";
+import { Metadata } from "next";
+import { notFound, redirect } from "next/navigation";
+import React from "react";
+export interface ChatPageProps {
+  params: {
+    id: string;
+  };
+}
+
+export async function generateMetadata({
+  params,
+}: ChatPageProps): Promise<Metadata> {
+  const user = await getUser();
+
+  if (!user) {
+    return {};
+  }
+
+  const chat = await getChat(params.id, user.id);
+  return {
+    title: chat?.title.toString().slice(0, 50) ?? "Chat",
+  };
+}
+
+async function Layout({
+  children,
+  params,
+}: {
+  readonly children: React.ReactNode;
+  readonly params: { id: string };
+}) {
+  const user = await getUser();
+
+  if (!user) {
+    if (params.id) {
+      return redirect(`/auth/login?next=/chat/${params.id}`);
+    }
+    redirect(`/auth/login?next=/chat`);
+  }
+
+  const chat = await getChat(params.id, user.id);
+
+  if (!chat) {
+    notFound();
+  }
+
+  if (chat?.userId !== user.email) {
+    notFound();
+  }
+
+  return (
+    <main className='relative h-full'>
+      {children}
+
+      <div className='z-10 absolute bottom-0 inset-x-0'>
+        <ChatPanel id={chat?.id} initialMessages={chat?.messages} />
+      </div>
+    </main>
+  );
+}
+
+export default Layout;
---
+++ app/chat/layout.tsx
@@ -0,0 +1,44 @@
+import MobileAppBar from "../components/ui/MobileAppBar";
+import { DesktopSideBar } from "../components/ui/SideBar";
+import ThemeToggler from "../components/ui/ThemeToggler";
+import ShareChat from "../components/ui/ShareChat";
+import { auth } from "../lib/auth";
+import { redirect } from "next/navigation";
+
+export default async function Layout({
+  children,
+}: {
+  readonly children: React.ReactNode;
+}) {
+  const user = await auth();
+
+  if (!user) {
+    redirect(`/auth/login?next=/chat`);
+  }
+
+  return (
+    <main className=''>
+      {/* AppBar For Mobile */}
+      <div className='block lg:hidden'>
+        <MobileAppBar />
+      </div>
+
+      {/* SideBar For Desktop */}
+
+      <div className='grid grid-cols-1 lg:grid-cols-8 xl:grid-cols-10 relative '>
+        <div className='hidden lg:block lg:col-span-3  dark:bg-black'>
+          <DesktopSideBar />
+        </div>
+
+        <div className='lg:col-span-5 xl:col-span-7 relative min-h-[90dvh]'>
+          <div className='absolute right-8 lg:right-14 top-8 z-50 flex items-center space-x-4 lg:space-x-6'>
+            <ShareChat />
+            <ThemeToggler>Switch Theme </ThemeToggler>
+          </div>
+
+          {children}
+        </div>
+      </div>
+    </main>
+  );
+}
