7c75d92c3b764c99ef2f36fd534f834f559e4372
ruckyTheWebDesigner <rukewejoseph77@gmail.com>
2023-11-29 02:32:41 +0100

    Add Button component to ThemeToggler and create QueryClientProvider component

--- app/components/ui/ThemeToggler.tsx
+++ app/components/ui/ThemeToggler.tsx
@@ -1,17 +1,25 @@
 "use client";
 
 import useThemeToggler from "@/app/lib/hooks/useThemeToggler";
-import { ActionIcon, Tooltip } from "@mantine/core";
+import { ActionIcon, Button, Tooltip } from "@mantine/core";
 import { BiMoon, BiSun } from "react-icons/bi";
 
-function ThemeToggler() {
+function ThemeToggler({ children }: { children?: React.ReactNode }) {
   const { theme, toggleTheme } = useThemeToggler();
 
   return (
     <Tooltip label={"Switch Mode"}>
-      <ActionIcon variant='default' onClick={toggleTheme}>
-        {theme === "dark" ? <BiMoon /> : <BiSun />}
-      </ActionIcon>
+      <Button
+        variant='default'
+        onClick={toggleTheme}
+        rightSection={
+          <ActionIcon variant='transparent' color='gray' radius='xl'>
+            {theme === "dark" ? <BiMoon /> : <BiSun />}
+          </ActionIcon>
+        }>
+        {/* {theme === "dark" ? <BiMoon /> : <BiSun />} */}
+        {children}
+      </Button>
     </Tooltip>
   );
 }
