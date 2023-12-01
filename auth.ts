6f930cd2d7ce1c85f2cf20e5499c3390640338c6
ruckyTheWebDesigner <rukewejoseph77@gmail.com>
2023-12-01 23:39:45 +0100

    Fix import in auth.ts and add PrismaAdapter in prisma/index.ts

--- auth.ts
+++ auth.ts
@@ -3,13 +3,13 @@ import NextAuth from "next-auth";
 // import Apple from "next-auth/providers/apple"
 import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
-import { PrismaAdapter } from "@auth/prisma-adapter";
 
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 import type { NextAuthConfig } from "next-auth";
+import { PrismaAdapter } from "./prisma";
 
 export const config = {
   adapter: PrismaAdapter(prisma),
