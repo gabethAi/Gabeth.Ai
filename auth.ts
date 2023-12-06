897b4a3471b18a0ff64f57ec835ccd3598b30a01
ruckyTheWebDesigner <rukewejoseph77@gmail.com>
2023-12-06 03:26:58 +0100

    Update authentication providers and adapters

--- auth.ts
+++ auth.ts
@@ -3,22 +3,49 @@ import NextAuth from "next-auth";
 // import Apple from "next-auth/providers/apple"
 import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
-
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
+import CredentialsProvider from "next-auth/providers/credentials";
 
 import type { NextAuthConfig } from "next-auth";
-import { PrismaAdapter } from "./prisma";
+import { loginUser } from "./lib/actions";
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import { db } from "./lib/db/drizzle";
 
 export const config = {
-  adapter: PrismaAdapter(prisma),
+  adapter: DrizzleAdapter(db),
+  debug: true,
   providers: [
     // Apple,
     GitHub,
     Google({
       clientId: process.env.AUTH_GOOGLE_ID,
       clientSecret: process.env.AUTH_GOOGLE_SECRET,
+    }),
+    CredentialsProvider({
+      // The name to display on the sign in form (e.g. "Sign in with...")
+      id: "credentials",
+      name: "Credentials",
+      // `credentials` is used to generate a form on the sign in page.
+      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+      // e.g. domain, username, password, 2FA token, etc.
+      // You can pass any HTML attribute to the <input> tag through the object.
+      async authorize(credentials, req) {
+        console.log(credentials, "credentials");
+        // Add logic here to look up the user from the credentials supplied
+        const user = await loginUser({
+          email: credentials.email as string,
+          password: credentials.password as string,
+        });
+
+        if (user) {
+          // Any object returned will be saved in `user` property of the JWT
+          return user;
+        } else {
+          // If you return null then an error will be displayed advising the user to check their details.
+          return null;
+
+          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+        }
+      },
     }),
   ],
   secret: process.env.AUTH_SECRET,
