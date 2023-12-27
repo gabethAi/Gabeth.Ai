794e69ba9a58992c1d071d9d27b2cec5068dbff8
ruckyTheWebDesigner <rukewejoseph77@gmail.com>
2023-12-28 00:38:25 +0100

    Refactor chat route and update layout metadata

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -22,75 +22,81 @@ export async function POST(req: Request) {
   const chatId = json.id || nanoid(10);
   const title = json.messages[0].content.substring(0, 100);
 
-  // Request the OpenAI API for the response based on the prompt
-  const response = await openai.chat.completions.create({
-    model: "gpt-3.5-turbo",
-    stream: true,
-    messages: messages,
-    temperature: 0.7,
-    max_tokens: 500,
-    top_p: 1,
-    frequency_penalty: 1,
-    presence_penalty: 1,
-  });
+  try {
+    // Request the OpenAI API for the response based on the prompt
+    const response = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      stream: true,
+      messages: messages,
+      temperature: 0.7,
+      max_tokens: 500,
+      top_p: 1,
+      frequency_penalty: 1,
+      presence_penalty: 1,
+    });
 
-  // Convert the response into a friendly text-stream
-  const stream = OpenAIStream(response, {
-    async onCompletion(completion) {},
-    async onFinal(completion) {
-      const path = `/chat/${chatId}`;
+    // Convert the response into a friendly text-stream
+    const stream = OpenAIStream(response, {
+      async onCompletion(completion) {},
+      async onFinal(completion) {
+        const path = `/chat/${chatId}`;
 
-      try {
-        const savedChat = await getChatById(chatId);
+        try {
+          const savedChat = await getChatById(chatId);
 
-        /**
-         * Saves the chat to the database if it doesn't already exist.
-         * @param chatId - The ID of the chat.
-         * @param title - The title of the chat.
-         * @param path - The path of the chat.
-         */
-        if (!savedChat) {
-          await saveChatToDb({
-            id: chatId,
-            title,
-            createdAt: new Date(),
-            path: path,
-          });
+          /**
+           * Saves the chat to the database if it doesn't already exist.
+           * @param chatId - The ID of the chat.
+           * @param title - The title of the chat.
+           * @param path - The path of the chat.
+           */
+          if (!savedChat) {
+            await saveChatToDb({
+              id: chatId,
+              title,
+              createdAt: new Date(),
+              path: path,
+            });
+          }
+
+          // get the last message from the messages array
+          const lastMessage = messages[messages.length - 1];
+
+          const messagesToInsert = [
+            {
+              id: nanoid(10),
+              chatId: chatId,
+              role: lastMessage.role,
+              content: lastMessage.content,
+              createdAt: new Date(),
+            },
+            {
+              id: nanoid(10),
+              chatId: chatId,
+              role: "assistant",
+              content: completion,
+              createdAt: new Date(Date.now() + 1000),
+            },
+          ];
+
+          for (const message of messagesToInsert) {
+            // save each message to the database
+            await addMessageToDb(message);
+          }
+        } catch (error) {
+          console.error("Error saving chat:", error);
+          throw error;
         }
+      },
+    });
 
-        // get the last message from the messages array
-        const lastMessage = messages[messages.length - 1];
+    // Respond with the stream
+    return new StreamingTextResponse(stream);
+  } catch (error: any) {
+    console.log(error, "error");
 
-        const messagesToInsert = [
-          {
-            id: nanoid(10),
-            chatId: chatId,
-            role: lastMessage.role,
-            content: lastMessage.content,
-            createdAt: new Date(),
-          },
-          {
-            id: nanoid(10),
-            chatId: chatId,
-            role: "assistant",
-            content: completion,
-            createdAt: new Date(Date.now() + 1000),
-          },
-        ];
-
-        for (const message of messagesToInsert) {
-          // save each message to the database
-          await addMessageToDb(message);
-        }
-      } catch (error) {
-        console.error("Error saving chat:", error);
-        throw error;
-      }
-    },
-  });
-
-  // Respond with the stream
-  return new StreamingTextResponse(stream);
+    throw error;
+  }
 }
 
 export async function GET(request: NextRequest) {
